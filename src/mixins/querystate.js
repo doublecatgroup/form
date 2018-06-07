d973a63a1090c02a4f507b43ccf49d6f1fccc46c
Double Cat Group <github@doublecatgroup.com>
2018-06-07 15:06:32 +0800

    Remove qdata support

--- src/mixins/querystate.js
+++ src/mixins/querystate.js
@@ -1,14 +1,5 @@
 const R = require('ramda')
 
-export function normalizeEntry (entry) {
-  const path = Array.isArray(entry.path) ? entry.path : [entry.path]
-  return R.assoc('path', path, entry)
-}
-
-function normalizeEntries (entries) {
-  return R.map(normalizeEntry, entries)
-}
-
 function qget (path) {
   return R.path(path, this.$route.query)
 }
@@ -31,20 +22,5 @@ export default {
   beforeCreate: function () {
     const methods = { qget, qset, qdel }
     this.$options.methods = R.merge(this.$options.methods || {}, methods)
-    const fn = this.$options.qdata
-    if (fn) {
-      const computed = {}
-      const entries = normalizeEntries(fn.call(this.$options.propsData))
-      for (const name in entries) {
-        const entry = entries[name]
-        const get = () => qget.call(this, entry.path)
-        const set = R.curry(qset)(entry.path)
-        computed[name] = { get, set }
-        if (get() === undefined) {
-          set.call(this, entry.def, true)
-        }
-      }
-      this.$options.computed = R.merge(this.$options.computed || {}, computed)
-    }
   }
 }
--- test/unit/specs/mixins/querystate.spec.js
+++
@@ -1,8 +0,0 @@
-import { normalizeEntry } from '@/mixins/querystate'
-
-describe('normalizeEntry', () => {
-  it('normalizes entries', () => {
-    const entry = { path: 'foo', def: 23 }
-    expect(normalizeEntry(entry)).toEqual({ path: ['foo'], def: 23 })
-  })
-})
